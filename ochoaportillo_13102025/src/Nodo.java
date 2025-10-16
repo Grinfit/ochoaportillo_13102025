192b6de523cb7dd30ce304c79d052db02c04cb34
Henry Arturo Ochoa Portillo <henry0020503@gmail.com>
2025-10-15 23:41:29 -0600

    Entrega de práctica 13102025

---
+++ ochoaportillo_13102025/src/Nodo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Nodo {
+    
+    int data;
+    Nodo siguiente;
+    
+    
+    public Nodo(int data){
+        this.data = data;
+        this.siguiente = null;
+    }
+    
+    
+}
